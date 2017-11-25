91fb9de6f280edb16bb79f3c5a1d9fa77a4317b4
andrewnguyen94 <andrewnguyen281194@gmail.com>
2017-11-25 16:39:16 +0700

    25/11/2017 di an cuoi roi ve que

--- lightbulb.cpp
+++ lightbulb.cpp
@@ -1,5 +1,6 @@
 #include "lightbulb.h"
 #include <iostream>
+#include <vector>
 
 struct VertexData
 {
@@ -10,9 +11,13 @@ struct VertexData
 
 LightBulb::LightBulb(std::vector<glm::vec3> out_vertices, std::vector<glm::vec2> out_uvs, std::vector<glm::vec3> out_normal)
 {
+    initializeOpenGLFunctions();
     this->out_vertices = out_vertices;
     this->out_uvs = out_uvs;
     this->out_normals = out_normal;
+    this->m_count = 0;
+
+    initLighBulbGeometry();
 }
 
 LightBulb::~LightBulb()
@@ -27,7 +32,21 @@ void LightBulb::drawLighBulb(QOpenGLShaderProgram *program)
 
 void LightBulb::initLighBulbGeometry()
 {
-
+    m_data.resize(this->out_vertices.size() * 3 +
+                  this->out_uvs.size() * 2 +
+                  this->out_normals.size() * 3);
+    GLfloat *p = m_data.data() + m_count;
+    for(unsigned int i = 0; i < this->out_vertices.size(); i++){
+        *p++ = this->out_vertices[i].x;
+        *p++ = this->out_vertices[i].y;
+        *p++ = this->out_vertices[i].z;
+        *p++ = this->out_uvs[i].x;
+        *p++ = this->out_uvs[i].y;
+        *p++ = this->out_normals[i].x;
+        *p++ = this->out_normals[i].y;
+        *p++ = this->out_normals[i].z;
+        m_count += 8;
+    }
 }
 
 
